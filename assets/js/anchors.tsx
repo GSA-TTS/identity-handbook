a14a1cfd9306e56fffacc411b46a7267a7e13a8b
Zach Margolis <zachary.margolis@gsa.gov>
2022-03-22 11:54:41 -0700

    lintfix

--- assets/js/anchors.tsx
+++ assets/js/anchors.tsx
@@ -1,5 +1,7 @@
 import Anchor from "anchor-js";
 
 export const loadAnchors = () => {
-  new Anchor().add('#main-content h2, #main-content h3, #main-content [data-anchor]');
-}
+  new Anchor().add(
+    "#main-content h2, #main-content h3, #main-content [data-anchor]"
+  );
+};
