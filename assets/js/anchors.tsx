a5256999037c1e271847badd780de7ae68d5f407
Zach Margolis <zachmargolis@users.noreply.github.com>
2022-03-22 14:07:27 -0700

    modulify other JS on the page (#233)
    * modulify other JS on the page
    
    * lintfix
    
    * Add --splitting
    
    Co-authored-by: Andrew Duthie <aduth@users.noreply.github.com>
    
    * Fix build path
    
    * Try to override typings... just cast as any
    
    * Add typings for anchor-js
    
    * Add `Anchor#add` types
    
    Co-authored-by: Andrew Duthie <aduth@users.noreply.github.com>
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>

---
+++ assets/js/anchors.tsx
@@ -0,0 +1,7 @@
+import Anchor from "anchor-js";
+
+export const loadAnchors = () => {
+  new Anchor().add(
+    "#main-content h2, #main-content h3, #main-content [data-anchor]"
+  );
+};
---
+++ assets/js/private-links.tsx
@@ -0,0 +1,40 @@
+import PrivateEye from "@18f/private-eye";
+
+export const loadPrivateEye = () => {
+  PrivateEye({
+    defaultMessage: "This link is private to TTS.",
+    ignoreUrls: [
+      "18f.slack.com",
+      "anywhere.gsa.gov",
+      "bookit.gsa.gov",
+      "calendar.gsa.gov",
+      "cm-jira.usa.gov",
+      "connect.gsa.gov",
+      "docs.google.com",
+      "drive.google.com",
+      "ea.gsa.gov",
+      "email.gsa.gov",
+      "gcims.gsa.gov",
+      "github.com/18f/identity-devops",
+      "github.com/18f/identity-devops-private",
+      "github.com/18f/identity-handbook-private",
+      "github.com/18f/identity-idp-config",
+      "gkey.gsa.gov",
+      "gsa-tts.slack.com",
+      "gsa.my.salesforce.com",
+      "gsaolu.gsa.gov",
+      "hrprod.hr.gsa.gov",
+      "insite.gsa.gov",
+      "login-gov.app.opsgenie.com",
+      "login-handbook.app.cloud.gov",
+      "mail.gsa.gov",
+      "meet.gsa.gov",
+      "pages-internal.18f.gov",
+      "one.newrelic.com",
+      "rpm.newrelic.com",
+      "tock.18f.gov",
+      "figma.com",
+      "dashboard.fr.cloud.gov",
+    ],
+  });
+};
