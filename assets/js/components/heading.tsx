1c2b72f87d2c22c71f417463c5ef03a7e7b99759
Andrew Duthie <andrew.duthie@gsa.gov>
2022-09-27 16:45:31 -0400

    Refactor buildHeader to use Heading component

--- assets/js/components/heading.tsx
+++ assets/js/components/heading.tsx
@@ -1,7 +1,7 @@
 import { h } from "preact";
 import { urlify, AnchorLink } from "./anchor-link";
 
-type HeadingLevel = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+export type HeadingLevel = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
 
 interface HeadingProps {
   level: HeadingLevel;
