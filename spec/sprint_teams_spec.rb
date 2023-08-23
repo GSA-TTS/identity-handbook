12b4cd4b906e0075652f8b46773fbff90a77c712
Andrew Duthie <andrew.duthie@gsa.gov>
2023-08-23 15:25:51 -0400

    Aggregate failures
    Co-authored-by: Zach Margolis <zachmargolis@users.noreply.github.com>

--- spec/sprint_teams_spec.rb
+++ spec/sprint_teams_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe '_data/sprint_teams.yml' do
   data = YAML.load_file('_data/sprint_teams.yml')
   data.each do |team|
     describe team['name'] do
-      it 'includes required properties' do
+      it 'includes required properties', :aggregate_failures do
         expect(team['name']).not_to be_nil
         expect(team['focus_area']).not_to be_nil
         expect(team['slack_channel_name']).not_to be_nil
