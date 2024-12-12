4412efa8ef1217b13d9ce8e65be23b4fae68dab1
Andrew Duthie <1779930+aduth@users.noreply.github.com>
2024-12-12 09:56:37 -0500

    Remove inherent padding from breadcrumb (#642)
    * Remove inherent padding from breadcrumb
    
    * Apply tag_options to rendered breadcrumb container

--- _components/breadcrumb_component.rb
+++ _components/breadcrumb_component.rb
@@ -1,1 +1,11 @@
-class BreadcrumbComponent < BaseComponent; end
+class BreadcrumbComponent < BaseComponent
+  attr_reader :tag_options
+
+  def initialize(**tag_options)
+    @tag_options = tag_options
+  end
+
+  def css_class
+    ['usa-breadcrumb', *tag_options[:class]]
+  end
+end
